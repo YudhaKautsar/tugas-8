859a38f11b74159fe16b35086cebd580cbf6a5d5
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-15 15:29:26 +0700

    first commit

---
+++ tugas-8.js
@@ -0,0 +1,13 @@
+const klub = {  
+    nama: 'AC Milan',
+    fans: 5000000,
+    stadium: 'San Siro',
+}
+
+console.log(klub);
+
+klub.nama = 'Real Madrid';
+klub.fans = 2000000000;
+klub.stadium = 'Santiago Bernabeu';
+
+console.log(klub);
